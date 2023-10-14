28d2b05145ea65651ca3474d7ee08396ba4cbce2
TheDavidFW <david.feng@utp.ac.pa>
2023-10-14 03:19:42 -0500

    console.log agregado al fetch

--- lab7/index.js
+++ lab7/index.js
@@ -9,6 +9,7 @@ fetch(url)
     nombre.innerHTML = `${data.data[3].name}`;
     colegio.innerHTML = `${data.data[3].school}`;
     imagen.innerHTML = `<img src=${data.data[3].imageSchool}></img>`;
+    console.log(data);
 })
 .catch(err=>console.log(err))
 
